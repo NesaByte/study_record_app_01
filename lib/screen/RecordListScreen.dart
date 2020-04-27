ff268323b4afafeb0a96ddde3bc6613a430c6241
linnefromice <linnefromice@gmail.com>
2020-04-27 15:46:50 +0900

    add record list screen (mock version)

---
+++ lib/screen/RecordListScreen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class RecordListScreen extends StatefulWidget {
+  RecordListScreen({Key key, this.date}) : super(key: key);
+
+  final String date;
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<RecordListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.date),
+      ),
+      drawer: Drawer(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: Icon(Icons.book),
+                title: Text('Ruby on Rails'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Programming'),
+                    Text('AM10:00 ~ AM11:00'),
+                  ],
+                ),
+                trailing: Text('1h'),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(12.0),
+              child: ListTile(
+                leading: Icon(Icons.book),
+                title: Text('初めてのGraphQL'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Reading'),
+                    Text('AM 11:00 ~ PM 1:00'),
+                  ],
+                ),
+                trailing: Text('2h'),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        tooltip: 'Register',
+        onPressed: () => print('Click Register Button...'), // TODO: dummy
+      ),
+    );
+  }
+}

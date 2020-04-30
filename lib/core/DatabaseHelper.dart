1e33e406d6cab02dc45d990fdbd3d9c2ea4968e2
linnefromice <linnefromice@gmail.com>
2020-05-01 00:48:53 +0900

    add database helper (on the way)

---
+++ lib/core/DatabaseHelper.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final _dbName = 'study_record_one_db.db';
+  static final _dbVersion = 1;
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database _database;
+  Future<Database> getDatabase() async {
+    if (_database != null) return _database;
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  Future<Database> _initDatabase() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, _dbName);
+    return await openDatabase(
+      path,
+      version: _dbVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE RECORD (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        kind TEXT NOT NULL,
+        iconCodePoint INTEGER NOT NULL,
+        iconFontFamily TEXT NOT NULL,
+        fromDate TEXT NOT NULL,
+        toDate TEXT NOT NULL,
+        version INTEGER NOT NULL
+      )
+    ''');
+  }
+}

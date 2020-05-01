1af2328f6c40a4d7dced6af67c627883cb0b466d
linnefromice <linnefromice@gmail.com>
2020-05-01 14:07:33 +0900

    modify databasehelper

--- lib/core/DatabaseHelper.dart
+++ lib/core/DatabaseHelper.dart
@@ -23,7 +23,7 @@ class DatabaseHelper {
   Future<Database> _initDatabase() async {
     Directory documentDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentDirectory.path, _dbName);
-    // await deleteDatabase(path);
+    await deleteDatabase(path);
     return await openDatabase(
       path,
       version: _dbVersion,
@@ -44,5 +44,41 @@ class DatabaseHelper {
         version INTEGER NOT NULL
       );
     ''');
+  }
+
+  Future<int> insert(final Map<String, dynamic> row, final String table) async {
+    final Database db = await instance.getDatabase();
+    return await db.insert(table, row);
+  }
+
+  Future<List<Map<String, dynamic>>> selectAllRows(final String table) async {
+    final Database db = await instance.getDatabase();
+    return await db.query(table);
+  }
+
+  Future<Map<String, dynamic>> selectOneRow(final String primaryKeyName, final String primaryKeyValue, final String table) async {
+    final Database db = await instance.getDatabase();
+    final List result = await db.query(table, where: '$primaryKeyName = ?', whereArgs: [primaryKeyValue]);
+    if (result.length == 1) {
+      return result[0];
+    } else {
+      return null; // Unexpected
+    }
+  }
+
+  Future<int> countAllRows(final String table) async {
+    final Database db = await instance.getDatabase();
+    return Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM $table'));
+  }
+
+  Future<int> update(final Map<String, dynamic> row, final String primaryKeyName, final String table) async {
+    final Database db = await instance.getDatabase();
+    final primaryKey = row[primaryKeyName];
+    return await db.update(table, row, where: '$primaryKey = ?', whereArgs: [primaryKeyName]);
+  }
+
+  Future<int> delete(final String primaryKeyName, final String primaryKeyValue, final String table) async {
+    Database db = await instance.getDatabase();
+    return await db.delete(table, where: '$primaryKeyName = ?', whereArgs: [primaryKeyValue]);
   }
 }

4b9331be77c98748d4af18327909ee8647fa5b5d
linnefromice <linnefromice@gmail.com>
2020-05-09 03:57:33 +0900

    add Constant class

---
+++ lib/Constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static final Map<IconData, String> availableIcons = {
+    Icons.computer : "computer",
+    Icons.book : "book",
+    Icons.work : "work",
+    Icons.free_breakfast : "break"
+  };
+}
